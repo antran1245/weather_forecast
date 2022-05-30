24d3f5a0f25921d605981ee74043ebf508f08359
antran1245 <antran1245@gmail.com>
2022-05-30 15:24:33 -0700

    call of weather api

--- flask_app/controllers/weathers.py
+++ flask_app/controllers/weathers.py
@@ -1,6 +1,15 @@
 from flask_app import app
-from flask import render_template, request, session
+from flask import render_template, request, jsonify, redirect
+import os
+import requests
 
 @app.route('/')
 def main():
+    # r = requests.get(f"http://api.weatherapi.com/v1/forecast.json?key={os.environ.get('FLASK_APP_API_KEY')}&q=London&days=3&aqi=no&alerts=no")
+    # if r.status_code == 200:
+    # print(r.json())
+    return redirect('/forcast')
+
+@app.route('/forcast')
+def forcast():
     return render_template('index.html')
