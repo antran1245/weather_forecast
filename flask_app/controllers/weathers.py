27fcaf1ce584466aa59e696f0a74eefdc67b61a7
antran1245 <antran1245@gmail.com>
2022-05-30 18:40:13 -0700

    created card

--- flask_app/controllers/weathers.py
+++ flask_app/controllers/weathers.py
@@ -1,15 +1,16 @@
 from flask_app import app
-from flask import render_template, request, jsonify, redirect
+from flask import render_template, request, jsonify, redirect, url_for
 import os
 import requests
+import datetime
 
 @app.route('/')
 def main():
-    # r = requests.get(f"http://api.weatherapi.com/v1/forecast.json?key={os.environ.get('FLASK_APP_API_KEY')}&q=London&days=3&aqi=no&alerts=no")
-    # if r.status_code == 200:
-    # print(r.json())
-    return redirect('/forcast')
+    r = requests.get(f"http://api.weatherapi.com/v1/forecast.json?key={os.environ.get('FLASK_APP_API_KEY')}&q=London&days=3&aqi=no&alerts=no")
+    if r.status_code == 200:
+        return render_template('index.html', data=r.json())
+    return redirect(f"/error/{r.status_code}")
 
-@app.route('/forcast')
-def forcast():
-    return render_template('index.html')
+@app.route('/error/<int:code>')
+def error(code):
+    return f"<h1>Status Code:{code}</h1>"
