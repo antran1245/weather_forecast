38ac54714bfa4d4330d38c0d81d43cc9e32377e9
antran1245 <antran1245@gmail.com>
2022-05-30 20:48:11 -0700

    fix the date display

--- flask_app/controllers/weathers.py
+++ flask_app/controllers/weathers.py
@@ -8,7 +8,14 @@ import datetime
 def main():
     r = requests.get(f"http://api.weatherapi.com/v1/forecast.json?key={os.environ.get('FLASK_APP_API_KEY')}&q=London&days=3&aqi=no&alerts=no")
     if r.status_code == 200:
-        return render_template('index.html', data=r.json())
+        # for inx, x in enumerate(r.json()["forecast"]["forecastday"]):
+        #     r.json()["forecast"]["forecastday"][inx]["date"] = datetime.datetime.strptime(x["date"], "%Y-%m-%d").strftime("%d %b, %Y")
+        #     print(datetime.datetime.strptime(x["date"], "%Y-%m-%d").strftime("%d %b, %Y"))
+        #     print(r.json()["forecast"]["forecastday"][inx]["date"])
+        date = list(map(lambda x: datetime.datetime.strptime(x["date"], "%Y-%m-%d").strftime("%b %d, %Y"), r.json()["forecast"]["forecastday"]))
+        # print(val)
+        # print(r.json())
+        return render_template('index.html', data=r.json(), date=date)
     return redirect(f"/error/{r.status_code}")
 
 @app.route('/error/<int:code>')
